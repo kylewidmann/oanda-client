55da529ac79f5a4bf164c4b018371e7614c86985
Kyle Widmann <kyle.widmann@gmail.com>
2025-02-14 08:58:32 -0500

    Fix close position size check

--- oanda_client/oanda.py
+++ oanda_client/oanda.py
@@ -135,7 +135,7 @@ class Oanda(IClient):
         args = {}
         if _position.size > 0:
             args["longUnits"] = _position.size
-        elif _position < 0:
+        elif _position.size < 0:
             args["shortUnits"] = _position.size
         else:
             raise RuntimeError(f"Requested to close position for {instrument} with size 0.")
